ced99858beee1610e37b7acb1f8748100623e890
David Adeyemi Ogunye <oladimejid@gmail.com>
2022-11-10 05:22:04 +0100

    Add files via upload

---
+++ lib/app/models/model_service.dart
@@ -0,0 +1,11 @@
+class ModelService {
+  String? image;
+  String? name;
+  String? productName;
+  String? rating;
+  double? price;
+  int? quantity;
+
+  ModelService(this.image, this.name, this.productName, this.rating, this.price,
+      this.quantity);
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:home_service_provider/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: AppPages.routes,
+    );
+  }
+}
